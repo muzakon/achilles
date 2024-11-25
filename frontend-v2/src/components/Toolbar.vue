238173fe0416a16de61229ca281098757b17066f
muzakon <hasanmuzak@hotmail.com>
2024-11-26 00:24:39 +0300

    Update: Toolbar Modified

--- frontend-v2/src/components/Toolbar.vue
+++ frontend-v2/src/components/Toolbar.vue
@@ -1,31 +1,37 @@
 <template>
   <div
     ref="toolbarSection"
-    class="absolute top-5 transform -translate-x-1/2 left-1/2 z-[9999] bg-[#202020] p-1 rounded-full justify-center border border-white/10"
+    class="absolute top-5 transform -translate-x-1/2 left-1/2 z-[9999] bg-white p-1 rounded-full justify-center border border-white/10"
   >
     <div class="relative flex items-center">
       <div
-        class="p-2 rounded-full flex items-center justify-center hover:text-purple-500 cursor-pointer hover:bg-neutral-900"
+        class="p-2 rounded-full flex items-center justify-center hover:text-neutral-900 cursor-pointer hover:bg-neutral-200 transition duration-200"
         :class="{
-          'text-purple-500 bg-neutral-900': item.id === selectedItem,
+          '!text-neutral-900 bg-neutral-200': item.id === selectedItem,
+          'text-neutral-400': item.id !== selectedItem,
           'mr-1': index !== menuItems.length - 1,
         }"
         v-for="(item, index) in menuItems"
-        v-tooltip.bottom="item.title"
         @click="selectTool(item.id)"
       >
         <span class="material-symbols-outlined text-[24px]">
           {{ item.icon }}
         </span>
       </div>
-      <!-- <Divider layout="vertical" /> -->
+
+      <v-divider
+        class="mx-3 border-gray-400"
+        vertical
+        :thickness="1"
+        color="grey-darken-4"
+      ></v-divider>
+
       <div
-        class="p-2 rounded-full flex items-center justify-center hover:text-purple-500 cursor-pointer hover:bg-neutral-900"
+        class="p-2 rounded-full flex items-center justify-center text-neutral-400 hover:text-neutral-900 cursor-pointer hover:bg-neutral-200 transition duration-200"
         :class="{
           'mr-1': index !== menuButtons.length - 1,
         }"
         v-for="(item, index) in menuButtons"
-        v-tooltip.bottom="item.title"
       >
         <span class="material-symbols-outlined text-[24px]">
           {{ item.icon }}
@@ -69,10 +75,6 @@ const menuItems = ref([
 ]);
 
 const menuButtons = ref([
-  {
-    icon: "cached",
-    title: "Vary Image",
-  },
   {
     icon: "download",
     title: "Download",
